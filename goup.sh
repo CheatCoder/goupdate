1c0fe84087775262e1cfc26cb011e650adeffe65
CheatCoder <CheatCoder@users.noreply.github.com>
2021-12-15 15:00:59 +0100

    Updating to vs Shellcheck and remove bashings

--- goup.sh
+++ goup.sh
@@ -1,42 +1,44 @@
 #!/bin/sh
 
 
-if ! [ $(id -u) = 0 ]; then
+if ! [ "$(id -u)" = 0 ]; then
    echo "Run me as Root"
    exit 1
 fi
 
 
-function get_latest_version_url(){
-    url=$(curl -s "https://go.dev/dl/" |  grep -o /dl/[go1234567890\.]*\.linux-amd64.tar.gz | head -n1)
-    filename=$(echo "${url##*/}")
+get_latest_version_url(){
+    url="https://go.dev"$(curl -s "https://go.dev/dl/" |  grep -o "/dl/[go1234567890\.]*\.linux-amd64.tar.gz" | head -n1)
+    filename="${url##*/}"
 }
 
-function fresh_install(){
+fresh_install(){
     echo "No version found at /src/local/go install new ..."
-    curl -OL $url
-    tar -C /usr/local -xzf $filename
-    rm $filename
+    get_latest_version_url
+    curl -OL "$url"
+    tar -C /usr/local -xzf "$filename"
+    rm "$filename"
 
     echo "add >export PATH=$PATH:/usr/local/go/bin< to your .profile or .bash"
 }
 
-function update_go(){
+update_go(){
     echo "Updating go"
-    curl -OL $url
+    get_latest_version_url
+    curl -OL "$url"
     rm -rf /usr/local/go 2>/dev/null
-    tar -C /usr/local -xzf $filename
-    rm $filename
+    tar -C /usr/local -xzf "$filename"
+    rm "$filename"
 }
 
-function localversion(){
+localversion(){
     
-    if ! version=$(/usr/local/go/bin/go version| grep -o go[\.1234567890]* | tail -n1); then
+    if ! version=$(/usr/local/go/bin/go version| grep -o "go[\.1234567890]*" | tail -n1); then
         fresh_install
         exit
     fi
 
-    echo $version
+    echo "$version"
 }
 
 echo "Testing Local go version ..."
@@ -45,9 +47,8 @@ localversion
 echo "Testing remote version against local version"
 get_latest_version_url
 
-if [[ $filename == *$version* ]]; then
+if echo "$filename" | grep -q "$version" ; then
   echo "No new found ... exit"
   exit
 fi
-
 update_go
