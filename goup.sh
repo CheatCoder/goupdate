e02e96c879e11dc00b2edfde3334f6f18010e036
CheatCoder <stefan.sauer@tutanota.com>
2022-06-16 09:41:20 +0200

    Add comments a help message and version infos

--- goup.sh
+++ goup.sh
@@ -1,4 +1,99 @@
 #!/bin/sh
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+    ${SCRIPT_NAME} [-hv] [-x] [-o]
+#%
+#% DESCRIPTION
+#%    A simple Scritp to Update or Install the newest GO version.
+#%    
+#%
+#% OPTIONS
+#%    -o [file], --output=[file]    Set log file (default=/dev/null)
+#%                                  use DEFAULT keyword to autoname file
+#%                                  The default value is /dev/null. - TODO
+#%    -x, --ignorelock              Ignore if lock file exists
+#%    -h, --help                    Print this help
+#%    -v, --version                 Print script information
+#%
+#% EXAMPLES
+#%    ${SCRIPT_NAME} -o DEFAULT arg1 arg2
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version         ${SCRIPT_NAME} (CheatCoder) 0.0.1
+#-    author          CheatCoder
+#-    copyright       Copyright (c) CheatCoder
+#-    license         unlicense
+#-
+#================================================================
+#  HISTORY
+#     16/06/2022 : CheatCoder : script creating
+#     
+# 
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+#== needed variables ==#
+SCRIPT_HEADSIZE=$(head -200 ${0} |grep -n "^# END_OF_HEADER" | cut -f1 -d:)
+SCRIPT_NAME="$(basename ${0})"
+
+#== usage functions ==#
+usage() { printf "Usage: "; head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#+" | sed -e "s/^#+[ ]*//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g" ; }
+usagefull() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#[%+-]" | sed -e "s/^#[%+-]//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g" ; }
+scriptinfo() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#-" | sed -e "s/^#-//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g"; }
+
+
+
+while getopts ":i:vh" optname
+  do
+    case "$optname" in
+      "v" | "version")
+        scriptinfo
+        exit 0;
+        ;;
+      "h"|help)
+	usagefull
+        exit 0;
+        ;;
+      "x")
+	SKIP=true
+	;;
+      "?")
+        usage
+        exit 0;
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        exit 0;
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        exit 0;
+        ;;
+    esac
+  done
+
+# -----------------------------------------------------------------
+
+if [ !$SKIP ]; then
+	LOCK_FILE=/tmp/${SCRIPT_NAME}.lock
+
+	if [ -f "$LOCK_FILE" ]; then
+		echo "Script is already running"
+		exit
+	fi
+fi
+
+# -----------------------------------------------------------------
+trap "rm -f $LOCK_FILE" EXIT
+touch $LOCK_FILE 
+
+# -----------------------------------------------------------------
+#  SCRIPT LOGIC GOES HERE
+# -----------------------------------------------------------------
 
 
 if ! [ "$(id -u)" = 0 ]; then
